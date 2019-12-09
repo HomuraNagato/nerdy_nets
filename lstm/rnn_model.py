d35015e25792a3a031ce58e1a2fcdaf5935bbaae
Rashi Dhar <rdhar1997@gmail.com>
2019-12-09 18:59:48 -0500

    feat: add produce sentence

--- lstm/rnn_model.py
+++ lstm/rnn_model.py
@@ -73,3 +73,13 @@ class RNN_Seq2Seq(tf.keras.Model):
 		loss=tf.reduce_sum(tf.keras.losses.sparse_categorical_crossentropy(labels,prbs)*mask)
 		return loss
 
+	def produce_sentence(self, ori_paragraph, summary, prbs, reverse_vocab, sen_len):
+
+        decoded_symbols = np.argmax(prbs, axis=1)
+        decoded_sentence = [ reverse_vocab[x] for x in decoded_symbols ]
+        decoded_sentence = " ".join(decoded_sentence)
+        #ori_paragraph = " ".join([ reverse_vocab[x] for x in paragraph ])
+        ori_summary = " ".join([ reverse_vocab[x] for x in summary ])
+        print("original paragraph\n", ori_paragraph)
+        print("summary sentence\n", ori_summary)
+        print("decoded sentence\n", decoded_sentence)
