1276584987968d5a0f205c27ea7ab80ce583e9c5
Rashi Dhar <rdhar1997@gmail.com>
2019-12-10 20:52:22 -0500

    fix:

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -61,7 +61,8 @@ class LSTM_Seq2Seq(tf.keras.Model):
         # print(labels.shape)
         # print(prbs.shape)
         loss=tf.reduce_sum(tf.keras.losses.sparse_categorical_crossentropy(labels,prbs))
-        # print(prbs[1])
+        print(prbs[1])
+        print(loss)
         return loss
     def produce_sentence(self, ori_paragraph, summary, prbs, reverse_vocab, sen_len):
         decoded_symbols = np.argmax(prbs, axis=1)
