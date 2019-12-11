40b39434f43259d10003105f282611364ec783dd
Rashi Dhar <rdhar1997@gmail.com>
2019-12-10 19:16:25 -0500

    fix: comment print statements

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -49,8 +49,8 @@ class LSTM_Seq2Seq(tf.keras.Model):
         :param mask:  tensor that acts as a padding mask [batch_size x window_size]
         :return: the loss of the model as a tensor
         """
-        print(labels.shape)
-        print(prbs.shape)
+        # print(labels.shape)
+        # print(prbs.shape)
         loss=tf.reduce_sum(tf.keras.losses.sparse_categorical_crossentropy(labels,prbs)*mask)
         return loss
     def produce_sentence(self, ori_paragraph, summary, prbs, reverse_vocab, sen_len):
